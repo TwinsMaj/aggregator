b42d9e2a90ea3eede7453416ac70dfb30a275b14
Taiwo Maja <taiwom@klluster.com>
2022-06-11 16:58:33 +0300

    chore(postgres): add postgres database

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+const DB_NAME = process.env.DB_NAME || '';
+const DB_USER = process.env.DB_USER || '';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_HOST = process.env.DB_HOST || '';
+
+const DATABASE_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
 const devConfig = {
 	server: {
 		port: SERVER_PORT,
 		env: 'development',
+	},
+	db: {
+		url: DATABASE_URL,
 	},
 };
 
@@ -15,6 +24,9 @@ const testConfig = {
 	server: {
 		port: 3434,
 		env: 'test',
+	},
+	db: {
+		url: '',
 	},
 };
 
