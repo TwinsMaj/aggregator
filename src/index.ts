88e5d3395295d6b52e683bc58047d8f2592713c5
Taiwo Maja <taiwom@klluster.com>
2022-06-12 16:51:13 +0300

    chore(logger): add logger and api rules

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import express from 'express';
 import http from 'http';
 import { config } from './config';
 import Logger from './logger';
+import { apiRules } from './middleware/api-rules';
+import { requestLogger } from './middleware/request-logger';
 
 const app = express();
 
 const initServer = async () => {
+	// log request
+	app.use(requestLogger);
+
+	// define API Rules
+	app.use(apiRules);
+
 	http
 		.createServer(app)
 		.listen(config.server.port, () => Logger.info(`Server is running on port: ${config.server.port}`));
